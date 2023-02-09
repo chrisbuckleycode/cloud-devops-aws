115cd6ab74ae928d466e818b8644c323a8e149f0
Chris Buckley <62516125+chrisbuckleycode@users.noreply.github.com>
2023-02-09 23:02:53 +0100

    revert: cron frequency

--- lambda-eventbridge-cron-example/terraform/eventbridge.tf
+++ lambda-eventbridge-cron-example/terraform/eventbridge.tf
@@ -1,7 +1,7 @@
 resource "aws_cloudwatch_event_rule" "self_trigger_10m" {
   name                = "self_trigger_10m"
   description         = "Self trigger on automated schedule, every 10 minutes"
-  schedule_expression = "rate(2 minutes)"
+  schedule_expression = "rate(10 minutes)"
 }
 
 
